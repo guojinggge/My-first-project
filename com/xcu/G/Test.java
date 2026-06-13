627bc80e0220b374dd2f2e465a5f0024970c8dfa
guojingge <My-first-project>
2026-06-13 15:55:56 +0800

    第一版

--- com/xcu/G/Test.java
+++ com/xcu/G/Test.java
@@ -1,20 +1,8 @@
 package com.xcu.G;
 
-import java.util.Scanner;
-
 public class Test {
-    public Test() {
-        System.out.println("构造方法一被调用了。。");
-    }
-    public Test(int x) {
-        System.out.println("构造方法二被调用了。。");
-    }
-    public Test(boolean b) {
-        System.out.println("构造方法三被调用了。。");
-    }
     public static void main(String[] args) {
-        Test test=new Test(true);
-
+        System.out.println("第一版");
     }
 
 }
